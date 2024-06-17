162fcd5003d144214fbc1aef1f501f308d46e982
Vu <vu.le@myria.com>
2024-06-18 00:52:35 +0700

    fix overview page deploy

--- app/(course)/courses/[courseId]/overview/page.tsx
+++ app/(course)/courses/[courseId]/overview/page.tsx
@@ -1,33 +1,21 @@
-import CourseSideBar from "@/components/layout/CourseSideBar";
-import Topbar from "@/components/layout/Topbar";
-import { db } from "@/lib/db";
-import { auth } from "@clerk/nextjs/server";
+import { clerkClient } from "@clerk/nextjs/server";
+import Image from "next/image";
 import { redirect } from "next/navigation";
 
-const CourseDetailsLayout = async ({
-  children,
-  params,
-}: {
-  children: React.ReactNode;
-  params: { courseId: string };
-}) => {
-  const { userId } = auth();
+import { db } from "@/lib/db";
+import ReadText from "@/components/custom/ReadText";
+import SectionMenu from "@/components/layout/SectionMenu";
 
-  if (!userId) {
-    return redirect("/sign-in");
-  }
-
+const CourseOverview = async ({ params }: { params: { courseId: string } }) => {
   const course = await db.course.findUnique({
     where: {
       id: params.courseId,
+      isPublished: true,
     },
     include: {
       sections: {
         where: {
           isPublished: true,
-        },
-        orderBy: {
-          position: "asc",
         },
       },
     },
@@ -37,15 +25,59 @@ const CourseDetailsLayout = async ({
     return redirect("/");
   }
 
+  const instructor = await clerkClient.users.getUser(course.instructorId);
+
+  let level;
+
+  if (course.levelId) {
+    level = await db.level.findUnique({
+      where: {
+        id: course.levelId,
+      },
+    });
+  }
+
   return (
-    <div className="h-full flex flex-col">
-      <Topbar />
-      <div className="flex-1 flex">
-        <CourseSideBar course={course} studentId={userId} />
-        <div className="flex-1">{children}</div>
+    <div className="px-6 py-4 flex flex-col gap-5 text-sm">
+      <div className="flex justify-between">
+        <h1 className="text-2xl font-bold">{course.title}</h1>
+        <SectionMenu course={course} />
+      </div>
+
+      <p className="font-medium">{course.subtitle}</p>
+
+      <div className="flex gap-2 items-center">
+        <Image
+          src={
+            instructor.imageUrl
+              ? instructor.imageUrl
+              : "/avatar_placeholder.jpg"
+          }
+          alt={instructor.fullName ? instructor.fullName : "Instructor photo"}
+          width={30}
+          height={30}
+          className="rounded-full"
+        />
+        <p className="font-bold">Instructor:</p>
+        <p>{instructor.fullName}</p>
+      </div>
+
+      <div className="flex gap-2">
+        <p className="font-bold">Price:</p>
+        <p>${course.price}</p>
+      </div>
+
+      <div className="flex gap-2">
+        <p className="font-bold">Level:</p>
+        <p>{level?.name}</p>
+      </div>
+
+      <div className="flex flex-col gap-2">
+        <p className="font-bold">Description:</p>
+        <ReadText value={course.description!} />
       </div>
     </div>
   );
 };
 
-export default CourseDetailsLayout;
+export default CourseOverview;
