ea26ecb41510728051703e92e26507472b107f1d
Vu <vu.le@myria.com>
2024-06-18 00:46:59 +0700

    init v1 academy

---
+++ app/layout.tsx
@@ -0,0 +1,31 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import { ClerkProvider } from "@clerk/nextjs";
+
+import "./globals.css";
+import ToasterProvider from "@/components/providers/ToasterProvider";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Vu Le Academy",
+  description: "Empowering minds, shaping future",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <ClerkProvider>
+      <html lang="en">
+        <body className={inter.className}>
+          <link rel="icon" href="/favicon.png" sizes="any" />
+          <ToasterProvider />
+          {children}
+          </body>
+      </html>
+    </ClerkProvider>
+  );
+}
